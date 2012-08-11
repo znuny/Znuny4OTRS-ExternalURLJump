363ec98d787648235d2b44daa1293ca3e6f5d147
Martin Edenhofer <me@edenhofer.de>
2012-08-11 11:23:18 +0200

    Init version.

---
+++ Kernel/Modules/ExternalURLJump.pm
@@ -0,0 +1,43 @@
+# --
+# Kernel/Modules/ExternalURLJump.pm - jump to external URL 
+# Copyright (C) 2011 Znuny, http://znuny.com/
+# --
+# $Id: $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::ExternalURLJump;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.12 $) [1];
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for (qw(ParamObject DBObject LayoutObject LogObject ConfigObject TimeObject)) {
+        if ( !$Self->{$_} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
+        }
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $ExtURL = $Self->{ParamObject}->GetParam( Param => 'URL' );
+
+    return $Self->{LayoutObject}->Redirect( ExtURL => $ExtURL );
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+External URL Jump
+=================
+With this extention you will be able to add external links to OTRS agent and customer navigation bar.
+
+Configuration
+=============
+Via SysConfig-Settings
+
+SysConfig -> Znuny4OTRS-ExternalURLJump -> Frontend::Agent::ModuleRegistration
+SysConfig -> Znuny4OTRS-ExternalURLJump -> Frontend::Customer::ModuleRegistration 
+
+Just update:
+o Name: Your Link Name
+o Link: URL=http://host/some_page.html (only URL param)
+
+Installation
+============
+Download the package and install it via admin interface -> package manager.
+
+Prerequisites
+* OTRS 3.1
+* OTRS 3.0
+
+Download
+========
+For download see https://github.com/znuny/Znuny4OTRS-ExternalURLJump/downloads/
+
+Commercial Support
+==================
+For this extention and for OTRS in gerneral visit http://znuny.com. Looking forward to hear from you!
+
+Enjoy!
+
+ Your Znuny Team!
+ http://znuny.com
+
